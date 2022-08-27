9788f4ee452fe23c8c1520eb1c9194aab4ea0e19
Olyadtemesgen <olexyit@gmail.com>
2022-08-27 09:36:57 -0700

    add

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for x in range(10):
-    for y in range(x + 1 , 10):
+    for y in range(x + 1, 10):
         if str(x) + str(y) != '89':
-            print('{}{}'.format(x , y), end = ', ')
+            print('{}{}'.format(x,y), end=', ')
         else:
-            print('89\n')
+            print('89')
