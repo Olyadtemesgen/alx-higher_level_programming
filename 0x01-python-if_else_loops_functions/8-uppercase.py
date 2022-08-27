ebbef157a59ddf99a7a17368a885358d429c25ca
Olyadtemesgen <olexyit@gmail.com>
2022-08-27 09:47:56 -0700

    add

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for x in str:
+        if ord(x) < 91 and ord(x) > 64:
+            new_str += x
+        elif ord(x) < 123 and ord(x) >96:
+            x = chr(ord(x) - 32)
+            new_str += x
+        else:
+            new_str += x
+    print(new_str)
+
