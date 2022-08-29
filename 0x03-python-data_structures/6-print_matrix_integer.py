3d6550909fa695609af24274ed39e2b2dba7e597
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-08-29 20:00:11 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        print("")
+        return
     for x in range(len(matrix)):
         for y in matrix[x]:
             if y != matrix[x][len(matrix[x]) - 1]:
