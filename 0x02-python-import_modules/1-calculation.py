0e1c44319e2bc698fa6e6eaf1157dfe63f017840
Olyadtemesgen <olexyit@gmail.com>
2022-08-29 06:51:49 -0700

    add

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    from 1-calculation import add, div, mul, sub
+    from calculator_1 import add, div, mul, sub
     a = 10
     b = 5
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
