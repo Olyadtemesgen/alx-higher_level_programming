b87d5d1559d5c379a248a3a47bf3b46379cd7860
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-08-30 23:09:48 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,20 +5,20 @@ if __name__ == '__main__':
     l = len(sys.argv)
     if l != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        exit('1')
     else:
         if sys.argv[2]  not in '+-*/':
             print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+            exit('1')
         else:
             a = int(sys.argv[1])
             b = int(sys.argv[3])
             if sys.argv[2] == '+':
-                print(' {:d} {} {:d} = {:d}'.format(a, sys.argv[2], b, add(a, b) ))
+                print('{:d} {} {:d} = {:d}'.format(a, sys.argv[2], b, add(a, b) ))
             elif sys.argv[2] == '-':
-                print(' {:d} {} {:d} = {:d}'.format(a, sys.argv[2], b, sub(a, b) ))
+                print('{:d} {} {:d} = {:d}'.format(a, sys.argv[2], b, sub(a, b) ))
             elif sys.argv[2] == '*':
-                print(' {:d} {} {:d} = {:d}'.format(a, sys.argv[2], b, mul(a, b) ))
+                print('{:d} {} {:d} = {:d}'.format(a, sys.argv[2], b, mul(a, b) ))
             elif sys.argv[2] == '/':
-                print(' {:d} {} {:d} = {:d}'.format(a, sys.argv[2], b, div(a, b) ))
-            exit(0)
+                print('{:d} {} {:d} = {:d}'.format(a, sys.argv[2], b, div(a, b) ))
+            exit('0')
