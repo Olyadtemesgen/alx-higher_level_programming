3d06116cc38523d2156a2469738cfa8967792e01
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-08-30 23:23:41 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    from calculator_1 import add, sub, mul, div
-    import sys
-    l = len(sys.argv)
-    if l != 4:
+from calculator_1 import add, sub, mul, div
+
+
+def arg_calc(argv):
+    n = len(argv) - 1
+    if n != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
+    if op == '+':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
+    elif op == '-':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
+    elif op == '*':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
+    elif op == '/':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
     else:
-        if sys.argv[2]  not in '+-*/':
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        else:
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            if sys.argv[2] == '+':
-                print('{:d} {} {:d} = {:d}'.format(a, sys.argv[2], b, add(a, b) ))
-            elif sys.argv[2] == '-':
-                print('{:d} {} {:d} = {:d}'.format(a, sys.argv[2], b, sub(a, b) ))
-            elif sys.argv[2] == '*':
-                print('{:d} {} {:d} = {:d}'.format(a, sys.argv[2], b, mul(a, b) ))
-            elif sys.argv[2] == '/':
-                print('{:d} {} {:d} = {:d}'.format(a, sys.argv[2], b, div(a, b) ))
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+
+if __name__ == "__main__":
+    import sys
+    arg_calc(sys.argv)
