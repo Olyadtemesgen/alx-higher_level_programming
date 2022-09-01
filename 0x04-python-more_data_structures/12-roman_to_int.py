d55f7d4a4c2f5eeee1e1f83ea8fd4edbf591f0f5
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-09-02 00:19:18 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
+def roman_to_int(roman_string: str):
     if roman_string is None or type(roman_string) != str:
         return 0
     data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     numbers = [data[x] for x in roman_string] + [0]
-    result = 0
-    for num in range(len(roman_string) - 1):
-        if numbers[num] >= numbers[num + 1]:
-            result += numbers[num]
+    rep = 0
+
+    for i in range(len(numbers) - 1):
+        if numbers[i] >= numbers[i+1]:
+            rep += numbers[i]
         else:
-            result -= numbers[num]
-    result += numbers[len(roman_number) - 1]
-    return result
+            rep -= numbers[i]
+
+    return rep
