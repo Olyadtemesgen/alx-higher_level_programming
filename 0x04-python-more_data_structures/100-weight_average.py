8db9fcb1667ebc037838048511d4fc7a565ef6b2
Olyadtemesgen <olexyit@gmail.com>
2022-09-01 13:25:04 -0700

    dd

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,11 +2,9 @@
 def weight_average(my_list=[]):
     sum = 0
     number = 0
+    if len(my_list) == 0:
+        return 0
     for x in range(len(my_list)):
         sum += my_list[x][0] * my_list[x][1]
         number += my_list[x][1]
-    if number != 0:
-        answer = sum / number
-        return number
-    else:
-        return 0
+    return number
