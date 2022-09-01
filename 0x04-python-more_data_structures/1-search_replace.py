b25e304adabc503d012b3b6707c71c5e2a312905
Olyadtemesgen <olexyit@gmail.com>
2022-09-01 13:07:36 -0700

    add

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -7,8 +7,3 @@ def search_replace(my_list, search, replace):
         else:
             new.append(replace)            
     return new
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
