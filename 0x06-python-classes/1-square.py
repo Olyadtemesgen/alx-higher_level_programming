dd7547222e553955117ac36e43385e10e378e84c
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-09-13 19:53:31 +0300

    commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,13 @@
 #usr/bin/python3
-
+"""
+This module defines everythin
+"""
 class Square:
-    
+    """
+    this is the other thig
+    """
     def __init__(self, size):
-        
+        """
+        this is the other
+        """
         self.__size = size
