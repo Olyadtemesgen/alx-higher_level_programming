11f6b953d945e5b100893501d2c167651a90d145
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-09-13 19:59:23 +0300

    commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
 class Square:
-    def __init__(self, size):
-        self.__size = size
+    def __init__(self, size=0):
+        from math import sqrt
+        try:
+            sqrt(size)
+            self.__size = size
+        except ValueError:
+            print("size must be >= 0")
+        except TypeError:
+            print("size must be an integer")
