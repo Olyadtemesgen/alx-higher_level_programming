99275f2a8cab46e6e942f09120236a3152ac6786
Olyadtemesgen <105017653+Olyadtemesgen@users.noreply.github.com>
2022-09-13 20:12:44 +0300

    cl

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,7 +2,7 @@
 """
 This module defines a Square class
 
-Its implements value and type checks for its attributes
+Its implements value and type checks for its attributes with area function
 """
 
 
@@ -10,8 +10,19 @@ class Square:
     """Square implementation
     """
     def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
         if type(size) != int:
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
