afe5233140dbaa0bb8c3b63810b04c9698d045f9
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2022-11-30 01:00:39 +0300

    fdasd

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-
-const arg = parseInt(process.argv[2]);
-
-if (arg) {
-  console.log('My number: ' + arg);
-} else {
+const convertArg = parseInt(process.argv[2]);
+if (isNaN(convertArg)) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + convertArg);
 }
